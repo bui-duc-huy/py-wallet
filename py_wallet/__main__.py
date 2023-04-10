2c3be4f82bc7f50a5e0b9bc9dc93eab236663465
bui-duc-huy <bui-duc-huy@users.noreply.github.com>
2023-04-10 10:47:28 +0000

    ✅ Ready to clone and code.

--- py_wallet/__main__.py
+++ py_wallet/__main__.py
@@ -1,4 +1,4 @@
-"""Entry point for project_name."""
+"""Entry point for py_wallet."""
 
 from .cli import main  # pragma: no cover
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for py_wallet package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("py_wallet", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="py_wallet",
+    version=read("py_wallet", "VERSION"),
+    description="Awesome py_wallet created by bui-duc-huy",
+    url="https://github.com/bui-duc-huy/py-wallet/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="bui-duc-huy",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["py_wallet = py_wallet.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
